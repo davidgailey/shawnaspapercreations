eb096af16bbe0c04d4c8b099d3c9dc4c5e2c32cd
David Gailey <dave.g.github@gmail.com>
2013-12-10 17:59:58 -0500

    added node modules, etc

---
+++ web.js
@@ -0,0 +1,44 @@
+var express = require('express'),
+	fs = require('fs'),
+	api = require('./facebook'),
+	port = process.env.PORT || 5000;
+
+var app = express();
+app.configure(function(){
+	app.use(express.logger());
+	app.use(express.compress());
+	app.use(express.methodOverride());
+	app.use(express.bodyParser());
+	app.use(express.static(__dirname));
+	app.use(express.errorHandler({
+		dumpExceptions: true, 
+		showStack: true
+	}));
+	app.use(app.router);
+});
+
+app.get('/', function(request, response) {
+	var indexContents = fs.readFile('index.html', function (err, data) {
+		if (err) throw err;
+		response.send(data.toString());
+	});
+});
+
+app.get('/allphotos', function(request, response) {
+
+	api.getAllPhotos();
+
+
+
+	/*
+		var indexContents = fs.readFile('index.html', function (err, data) {
+			if (err) throw err;
+			response.send(data.toString());
+		});
+	*/
+});
+
+
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
