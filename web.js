4fc3ab83abfb56ae914acf9207f348b73a8ced57
David Gailey <dave.g.github@gmail.com>
2013-12-19 07:10:34 -0500

    adding handlebar support - still debugging

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+// load modules
 var express = require('express'),
+	handlebars = require('express3-handlebars'),
 	fs = require('fs'),
 	fbapi = require('./facebook'),
-	port = process.env.PORT || 5000;
+	engines = require('consolidate');
 
+var port = process.env.PORT || 5001;
 var app = express();
+
+// configuration
 app.configure(function(){
 	app.use(express.logger());
 	app.use(express.compress());
@@ -15,20 +20,32 @@ app.configure(function(){
 		showStack: true
 	}));
 	app.use(app.router);
+
+	// handlebars config
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'handlebars');
+   	app.set("view options", { layout: false });
+   	app.engine('.handlebars', engines.handlebars);  //Set Extension here   
 });
 
+//routes
 app.get('/', function(request, response) {
-	var indexContents = fs.readFile('index.html', function (err, data) {
-		if (err) throw err;
-		response.send(data.toString());
-	});
+
+	var data = {title:"testing"};
+  	response.render("index.handlebars", data);  //Use Extension here
+
+	/*
+		var indexContents = fs.readFile('index.html', function (err, data) {
+			if (err) throw err;
+			response.send(data.toString());
+		});
+	*/
 });
 
 app.get('/allphotos', function(request, response) {
 
 	var imgSourceArray = fbapi.getAllPhotos(response);
 	//response.send('allphotos');
-
 
 	/*
 		var indexContents = fs.readFile('index.html', function (err, data) {
@@ -42,3 +59,35 @@ app.get('/allphotos', function(request, response) {
 app.listen(port, function() {
 	console.log("Listening on " + port);
 });
+
+
+
+/* http://pastebin.com/GpSsbqXv
+
+//Load Modules  
+var express = require('express');
+var handlebars = require('handlebars');
+var engines = require('consolidate');
+
+var app = express();
+
+// Configuration
+app.configure( function() {
+	app.set('views', __dirname + '/template');
+	app.set('view engine', 'handlebars');
+   	app.set("view options", { layout: false });
+   	app.engine('.handlebars', engines.handlebars);  //Set Extension here   
+});
+
+// Routes
+app.get('/', function(req, res) {
+  	var data = {title:"test"};
+  	res.render("profilecard.handlebars", data);  //Use Extension here
+});
+
+app.listen(3000);
+
+console.log("NodeJS Server Started");
+
+
+*/
